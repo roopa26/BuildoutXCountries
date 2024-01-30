7915929ba6a83f4daf49e72208be46a8ac448ab2
Roopa1 <Roopa1@nextgen.com>
2024-01-30 16:45:32 +0530

    X countries display code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import CountryCard from './components/Card/CountryCard';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+
+  const [countryData, setCountryData] = useState([]);
+
+  const endpoint = "https://restcountries.com/v3.1/all";
+
+  useEffect(()=>{
+    const fetchDataAndLog = async () => {
+      try {
+          const res = await fetchData();
+          setCountryData(res);
+      } catch (error) {
+          console.error(error);
+      }
+  };
+
+  fetchDataAndLog();
+  },[]);
+
+  const fetchData =async ()=>{
+    try{
+      const response = await fetch(endpoint);
+      return await response.json();
+    }
+    catch(ex){
+      console.log(ex);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrap'>
+      {countryData.map((item)=>(
+         <CountryCard id={item.name.common} countryFlag={item.flags.png} altName={item.flags.alt} countryName={item.name.official}/>   
+      ))}
     </div>
   );
 }
